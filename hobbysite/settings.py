7f832097bf4e17b6f776bac7be70d89ab6667dab
LamberlainMuli <109662687+LamberlainMuli@users.noreply.github.com>
2024-05-06 22:31:57 +0800

    Edited settings to add media root, url, login redirect, logout redirecdt, and email backend + edited urls.py to add the accounts routes

--- hobbysite/settings.py
+++ hobbysite/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 from pathlib import Path
 from dotenv import load_dotenv
 import os
+
+
 load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -123,7 +125,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
+
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR/'static'
+]
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR/"media"
+LOGIN_REDIRECT_URL = "/"
+LOGOUT_REDIRECT_URL = "/accounts/login"
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
