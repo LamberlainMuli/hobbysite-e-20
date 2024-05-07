682aa63dcb1cc5d3a7f93b5aeb1e3ebbdfc45b6f
Jacob Cano <157722126+CornOnTheKob@users.noreply.github.com>
2024-05-07 10:07:18 +0800

    Uploaded Initial Draft of Wiki Finals

--- wiki/admin.py
+++ wiki/admin.py
@@ -1,7 +1,15 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
 
-from .models import ArticleCategory, Article
+from user_management.models import User
 
+from .models import ArticleCategory, Article, Comment
+
+
+class CommentInline(admin.TabularInline):
+    model = Comment
+    extra = 0  # Number of empty forms to display
 
 class ArticleInline(admin.TabularInline):
     model = Article
@@ -9,9 +17,11 @@ class ArticleInline(admin.TabularInline):
 
 class ArticleAdmin(admin.ModelAdmin):
     model = Article
-    search_fields = ['title', 'category', 'entry' ,'created_on','updated_on']
+    search_fields = ['title', 'category', 'entry' ,'created_on','updated_on','image']
     list_filter = ['category']
-    list_display = ['title',  'category', 'entry' ,'created_on','updated_on']
+    list_display = ['title',  'category', 'entry' ,'created_on','updated_on', 'image']
+    inlines = [CommentInline]
+
 
 
 class ArticleCategoryAdmin(admin.ModelAdmin):
@@ -19,6 +29,9 @@ class ArticleCategoryAdmin(admin.ModelAdmin):
     inlines = [ArticleInline]
 
 
+
 admin.site.register(Article, ArticleAdmin)
 admin.site.register(ArticleCategory,ArticleCategoryAdmin)
+
+
 # Register your models here.
--- wiki/urls.py
+++ wiki/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 
-from .views import WikiListView, WikiDetailView, WikiIndex
+from .views import WikiListView, WikiDetailView, WikiCreateView, WikiUpdateView, WikiIndex
+from . import views
 
 urlpatterns = [
     path('articles', WikiListView.as_view(), name="wiki_list"), 
-    path('article/<int:pk>', WikiDetailView.as_view(), name = "wiki_detail"),
+    path('article/<int:pk>/', WikiDetailView.as_view(), name = "wiki_detail"),
+    path("article/add", WikiCreateView.as_view(), name="wiki_create"),
+    path('article/<int:pk>/edit', WikiUpdateView.as_view(), name = "wiki_update"),
     path('', WikiIndex.as_view(), name="index"),
     ]
 
