4c8db83892a2f7b179fa630249c70bb87da64479
LamberlainMuli <109662687+LamberlainMuli@users.noreply.github.com>
2024-05-07 23:20:53 +0800

    Completed frontend for article list, and article create templates

--- blog/views.py
+++ blog/views.py
@@ -31,25 +31,6 @@ class ArticleListView(ListView):
 class ArticleDetailView(DetailView):
     model = Article
     template_name = 'blog/article_detail.html'
-    
-class BlogIndex(ListView):
-    model = Article
-    template_name = 'blog/index.html'
-    context_object_name = 'article_list'
-    paginate_by = 5
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        articles = Article.objects.all()
-        categories = ArticleCategory.objects.all()
-        context['categories'] = categories
-        context['recent_articles'] = articles[:5]
-        context['latest_article'] = articles.first()
-        context['total_articles'] = articles.count()
-        context['total_words'] = sum(article.entry.count(' ')+1 for article in articles)
-        context['total_read_time'] = context['total_words']/200
-        context['average_read_time'] = sum((article.entry.count(' ')+1)/200 for article in articles) / context['total_articles'] if context['total_articles'] > 0 else 0
-        return context
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -104,3 +85,22 @@ class ArticleUpdateView(LoginRequiredMixin, UpdateView):
     def form_valid(self, form):
         form.instance.article = Article.objects.get(pk=self.kwargs["pk"])
         return super().form_valid(form)
+    
+class BlogIndex(ListView):
+    model = Article
+    template_name = 'blog/index.html'
+    context_object_name = 'article_list'
+    paginate_by = 5
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        articles = Article.objects.all()
+        categories = ArticleCategory.objects.all()
+        context['categories'] = categories
+        context['recent_articles'] = articles[:5]
+        context['latest_article'] = articles.first()
+        context['total_articles'] = articles.count()
+        context['total_words'] = sum(article.entry.count(' ')+1 for article in articles)
+        context['total_read_time'] = context['total_words']/200
+        context['average_read_time'] = sum((article.entry.count(' ')+1)/200 for article in articles) / context['total_articles'] if context['total_articles'] > 0 else 0
+        return context
