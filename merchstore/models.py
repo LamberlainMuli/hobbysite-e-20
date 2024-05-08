2d65f747678cb57f707318f2e3eac0fb4ad1f9ff
maigelazia <maigela.garcia@student.ateneo.edu>
2024-05-08 19:58:14 +0800

    Updated merchstore app

--- merchstore/models.py
+++ merchstore/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.urls import reverse
 
-# Create your models here.
+
 class ProductType(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
@@ -17,12 +17,32 @@ class ProductType(models.Model):
     
     def get_read_time(self):
         return self.get_word_count() / 200
-    
+
+
 class Product(models.Model):
+
+    product_status = {
+        "Available": "Available",
+        "On Sale": "On Sale",
+        "Out of Stock": "Out of Stock",
+    }
+
     name = models.CharField(max_length=255)
-    product_type = models.ForeignKey(ProductType, on_delete=models.SET_NULL, null=True, related_name='type')
+    type = models.ForeignKey(
+        ProductType,
+        on_delete=models.SET_NULL,
+        related_name="products",
+        null=True,
+    )
+    owner = models.ForeignKey(
+        "user_management.Profile",
+        on_delete=models.CASCADE,
+        related_name="products",
+    )
     description = models.TextField()
-    price = models.DecimalField(max_digits=6, decimal_places=2)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.IntegerField()
+    status = models.CharField(max_length=32, choices=product_status)
 
     class Meta:
         ordering = ['name']
@@ -38,4 +58,42 @@ class Product(models.Model):
     
     def get_read_time(self):
         return self.get_word_count() / 200
+    
+    def save(self):
+        if self.stock <= 0:
+            self.status = "Out of Stock"
+        else:
+            self.status = "Available"
+        return super().save()
+
+
+class Transaction(models.Model):
+
+    transaction_status = {
+        "On Cart": "On Cart",
+        "To Pay": "To Pay",
+        "To Ship": "To Ship",
+        "To Receive": "To Receive",
+        "Delivered": "Delivered",
+    }
+
+    buyer = models.ForeignKey(
+        "user_management.Profile",
+        on_delete=models.SET_NULL,
+        null=True,
+    )
+    product = models.ForeignKey(
+        Product,
+        on_delete=models.SET_NULL,
+        null=True,
+    )
+    amount = models.IntegerField()
+    status = models.CharField(max_length=32, choices=transaction_status)
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.amount} - {self.product} - {self.status}"
+    
+    def get_absolute_url(self):
+        return reverse("merchstore:transaction_detail", kwargs={"pk": self.pk})
     
